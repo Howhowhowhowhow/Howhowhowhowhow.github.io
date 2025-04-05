cd685d285db8bbe5d8f5cfbf737c92d2aa5691ea
HowCam <3604402972@qq.com>
2025-04-05 21:46:57 +0800

    Site updated: 2025-04-05 21:46:57

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/03/AED/","2025/03/01/漫谈DeepSeek：人工智能领域的新星/","2025/02/23/welcome/"];function toRandomPost(){
+var posts=["2025/04/03/AED/","2025/04/05/communication-log/","2025/03/01/漫谈DeepSeek：人工智能领域的新星/","2025/02/23/welcome/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
